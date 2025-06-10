7465404ee251efd28b2a0916cd26012871a33712
Ezekiel Leslie Omire <ezekielomire@gmail.com>
2025-06-10 04:04:23 +0300

    Defines currentUser and related greeting messages to pass all string lab tests

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Write your code in this file!
+
+// Define currentUser
+const currentUser = 'Grace Hopper';
+
+// Define welcomeMessage
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+// Define excitedWelcomeMessage
+const excitedWelcomeMessage = `WELCOME TO FLATBOOK, ${currentUser.toUpperCase()}!`;
+
+// Define shortGreeting
+const shortGreeting = `Welcome, ${currentUser[0]}!`;
+
